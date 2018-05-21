7a9fc3044882257d2f4ecc214e8ff8b972e4bf85
gagan <gagan.chhabra1990@gmail.com>
2018-05-21 20:03:07 +0530

    1 View Resolver 2. Update Handler

--- src/main/java/com/gagan/springmvc/LoginController.java
+++ src/main/java/com/gagan/springmvc/LoginController.java
@@ -3,14 +3,20 @@ package com.gagan.springmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-    @RequestMapping(name = "/login", method = RequestMethod.GET)
+/*
+    @RequestMapping(name = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public String hello(){
         return "Hello World";
+    }
+*/
+
+    @RequestMapping(name = "/login", method = RequestMethod.GET)
+    public String login(){
+        return "login";
     }
 }
