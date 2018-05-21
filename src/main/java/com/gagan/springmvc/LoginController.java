facf1d9204c743322207e686c0bd3497c40b3655
gagan <gagan.chhabra1990@gmail.com>
2018-05-21 19:21:52 +0530

    Initail Setup 1. Configure DispatcherServlet 2. Create LoginController

---
+++ src/main/java/com/gagan/springmvc/LoginController.java
@@ -0,0 +1,16 @@
+package com.gagan.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(name = "/login", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello(){
+        return "Hello World";
+    }
+}
